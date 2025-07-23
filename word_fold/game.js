1631d772262256007801b9c8e047c62839b95c5b
domenicaelg <domenicaelgart@gmail.com>
2025-07-23 16:05:23 -0400

    hi

--- word_fold/game.js
+++ word_fold/game.js
@@ -28,6 +28,14 @@ const BOARDS = [
     },
 ]
 
+let curBoard = 0
+let points = 0
+
+let gotWord = [false, false, false, false, false]
+
+let curWords = BOARDS[curBoard].words
+console.log("current words: " +curWords)
+
 function make_cell_list(){
     let cells = Array.from(document.getElementById("cell-holder").children)
     let cell_board = [];
@@ -49,7 +57,7 @@ function setup_game(board){
 }
 
 setup_game(BOARDS[0].cells);
-document.getElementById("words").innerHTML = "Words to spell: " + BOARDS[0].words.join(", ");
+document.getElementById("words").innerHTML = "Words to spell: " + curWords.join(", ");
 
 let selected_x = -1;
 let selected_y = -1;
@@ -57,22 +65,32 @@ let selected_y = -1;
 function select(x, y){
     let cell = CELLS[y][x];
     if(cell.innerHTML.length > 0) {
-        selected_x == x;
-        selected_y == y;
+        if(selected_x >= 0 && selected_y >= 0) {
+            CELLS[selected_y][selected_x].classList.remove("selected");
+        }
+        selected_x = x;
+        selected_y = y;
         cell.classList.add("selected");
     }
 }
 
-function move(x, y){
-
-}
-
 function unselect(x, y){
-
+    CELLS[y][x].classList.remove("selected");
+    selected_x = -1;
+    selected_y = -1;
 }
 
 function can_move(x, y){
+    let is_next_to = Math.abs(selected_x - x) + Math.abs(selected_y - y) == 1;
 
+    return selected_x >= 0 && selected_y >= 0 && is_next_to && CELLS[y][x].innerHTML.length > 0;
+
+}
+
+function move(x, y){
+    CELLS[y][x].innerHTML = CELLS[selected_y][selected_x].innerHTML + CELLS[y][x].innerHTML;
+    CELLS[selected_y][selected_x].innerHTML = "";
+    select(x, y);
 }
 
 function on_click(x, y){
@@ -83,4 +101,63 @@ function on_click(x, y){
     } else {
         select(x, y)
     }
+    if(CELLS[y][x].innerHTML == BOARDS[curBoard].words[0] && !gotWord[0]){
+        points += 50
+        document.getElementById("pointCount").innerHTML = "Points: " + points
+        gotWord[0] = true
+
+        // gotWord[0].classList.add(gotWord)
+        
+        document.getElementById("words").innerHTML = "Words to spell: " + BOARDS[0].curWords.join(", ");
+    }
+    if(CELLS[y][x].innerHTML == BOARDS[curBoard].words[1] && !gotWord[1]){
+        points += 50
+        document.getElementById("pointCount").innerHTML = "Points: " + points
+        gotWord[1] = true
+
+    }
+    if(CELLS[y][x].innerHTML == BOARDS[curBoard].words[2] && !gotWord[2]){
+        points += 50
+        document.getElementById("pointCount").innerHTML = "Points: " + points
+        gotWord[2] = true
+    }
+    if(CELLS[y][x].innerHTML == BOARDS[curBoard].words[3] && !gotWord[3]){
+        points += 50
+        document.getElementById("pointCount").innerHTML = "Points: " + points
+        gotWord[3] = true
+    }
+    if(CELLS[y][x].innerHTML == BOARDS[curBoard].words[4] && !gotWord[4]){
+        points += 50
+        document.getElementById("pointCount").innerHTML = "Points: " + points
+        gotWord[4] = true
+    }
+}
+
+function changeBoard(newBoard){
+
+    setup_game(BOARDS[newBoard].cells)
+    
+    document.getElementById("words").innerHTML = "Words to spell: " + BOARDS[newBoard].words.join(", ");
+
+    selected_x = -1;
+    selected_y = -1;
+
+    curBoard = newBoard
+
+}
+
+function Restart(){
+    
+    setup_game(BOARDS[curBoard].cells);
+    
+    document.getElementById("words").innerHTML = "Words to spell: " + BOARDS[curBoard].words.join(", ");
+    
+    CELLS[selected_y][selected_x].classList.remove("selected");
+    selected_x = -1;
+    selected_y = -1;
+   
+    points = 0;
+    document.getElementById("pointCount").innerHTML = "Points: " + points
+    gotWord = [false, false, false, false, false]
+
 }
