d817fab673bb3e647f014f40090d0db8494e7a3d
domenicaelg <domenicaelgart@gmail.com>
2025-07-23 10:15:25 -0400

    hi

--- rainbow.js
+++ rainbow.js
@@ -1,10 +1,21 @@
-let angle = 0;
-
+let angle = 300;
+let motion = false
 
 function onframe() {
-    console.log("Hello World")
-    
-    angle+=1
+    console.log("Hello World")    
+    if(motion==false)
+    {
+        angle += 0.5
+    }
+    else
+    {
+        angle -= 0.5
+    }
+    if(angle == 400)
+        motion = true
+    if(angle == 300)
+        motion = false
+
     let angle2 = angle + 180;
     document.body.style="background-color: hsl(" + angle +  "deg, 100%, 50%); --rotation: " + angle2 + "deg"
     
